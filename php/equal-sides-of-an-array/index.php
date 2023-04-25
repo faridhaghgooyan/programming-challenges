a97f699237fca934ccf979c5191f4508fe2456d1
Farid HaghGooyan <51539977+faridhaghgooyan@users.noreply.github.com>
2023-04-25 18:39:37 +0200

    Delete index.php

--- php/equal-sides-of-an-array/index.php
+++
@@ -1,9 +0,0 @@
-function find_even_index(array $arr) : int
-{
-    for( $i = 0 ; $i < count($arr) ; $i++ ){
-        $left = array_sum(array_slice($arr, 0, $i));
-        $right = array_sum(array_slice($arr, $i + 1));
-        if($left == $right) return $i;
-    }
-    return '-1';
-}
